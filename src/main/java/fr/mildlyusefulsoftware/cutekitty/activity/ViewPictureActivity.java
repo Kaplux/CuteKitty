4f353891e968a4cfb9bb3bce72f44c6a4892de45
Kaplux <kaplux@gmail.com>
2012-03-29 20:07:38 +0200

    fixed bugs on orientation change

--- src/main/java/fr/mildlyusefulsoftware/cutekitty/activity/ViewPictureActivity.java
+++ src/main/java/fr/mildlyusefulsoftware/cutekitty/activity/ViewPictureActivity.java
@@ -3,7 +3,6 @@ package fr.mildlyusefulsoftware.cutekitty.activity;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -28,6 +27,7 @@ public class ViewPictureActivity extends Activity {
 
 	private static String TAG = "cutekitty";
 	private AdView adView;
+	private int currentPosition = 0;
 
 	/**
 	 * Called when the activity is first created.
@@ -45,54 +45,74 @@ public class ViewPictureActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_LEFT_ICON);
 		setContentView(R.layout.view_picture_layout);
 		setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.icon);
-		initAdBannerView();
+		
 		Gallery pictureList = (Gallery) findViewById(R.id.pictureList);
-		loadImageFromPosition(0);
+		if (savedInstanceState != null) {
+			currentPosition = savedInstanceState.getInt("POSITION");
+			Log.d(TAG, "restore position " + currentPosition);
+			pictureList.setSelection(currentPosition, true);
+			loadImageFromPosition(currentPosition);
+		} else {
+			loadImageFromPosition(currentPosition);
+		}
 		pictureList.setAdapter(new ImageAdapter(this));
 		pictureList.setOnItemClickListener(new OnItemClickListener() {
-	        @Override
-			public void onItemClick(AdapterView parent, View v, int position, long id) {
-	        	loadImageFromPosition(position);
-	        }
-	    });
-			if (savedInstanceState != null) {
-				int pos = savedInstanceState.getInt("POSITION");
-				Log.d(TAG,"restore position "+pos);
-			//	pictureList.setSelection(pos);
-				//loadImageFromPosition(pos);
+			@Override
+			public void onItemClick(AdapterView parent, View v, int position,
+					long id) {
+				currentPosition = position;
+				loadImageFromPosition(position);
 			}
-		
-		}
-
+		});
+		initAdBannerView();
+	}
+	
 
 	private void loadImageFromPosition(final int pos) {
 		final Activity currentActivity = this;
-		final ProgressDialog progressDialog = ProgressDialog.show(this,
-				"Loading", "please wait", true);
+
 		new AsyncTask<Void, Void, Picture>() {
+
+			// ProgressDialog progressDialog=null;
+
 			@Override
 			protected Picture doInBackground(Void... params) {
-				return PicturePager.getInstance(currentActivity).getPictureAt(pos);
+				return PicturePager.getInstance(currentActivity).getPictureAt(
+						pos);
 			}
+
+			@Override
+			protected void onPreExecute() {
+				// progressDialog = ProgressDialog.show(currentActivity,
+				// "Loading", "please wait", true);
+				super.onPreExecute();
+			}
+
 			protected void onPostExecute(Picture picture) {
-				progressDialog.dismiss();
+				// if (progressDialog.isShowing()) {
+				// try {
+				// progressDialog.dismiss();
+				// } catch (Exception e) {
+				// Log.e(TAG, e.getMessage(), e);
+				// }
+				//
+				// }
 				Bitmap b;
 				try {
 					b = Picture.getBitmapFromPicture(picture);
-					ImageView pictureView=(ImageView) findViewById(R.id.pictureView);
+					ImageView pictureView = (ImageView) findViewById(R.id.pictureView);
 					pictureView.setImageBitmap(b);
 				} catch (IOException e1) {
-					Log.e(TAG,e1.getMessage(),e1);
+					Log.e(TAG, e1.getMessage(), e1);
 				}
 			}
 
 		}.execute();
 
-		
 	}
 
-	
 	protected void initAdBannerView() {
+		try{
 		final ViewGroup quoteLayout = (ViewGroup) findViewById(R.id.view_picutre_root_layout);
 		// Create the adView
 		adView = new AdView(this, AdSize.BANNER, "a14f6f84d0cc485");
@@ -100,14 +120,15 @@ public class ViewPictureActivity extends Activity {
 		adLayout.addView(adLayout);
 		// Add the adView to it
 		quoteLayout.addView(adView);
-		AdRequest ar=new AdRequest();
+		AdRequest ar = new AdRequest();
 		ar.addTestDevice(AdRequest.TEST_EMULATOR);
-		
+
 		// Initiate a generic request to load it with an ad
 		adView.loadAd(ar);
-
+		}catch (Exception e) {
+			Log.e(TAG,e.getMessage(),e);
+		}
 	}
-	
 
 	@Override
 	protected void onDestroy() {
@@ -116,13 +137,12 @@ public class ViewPictureActivity extends Activity {
 			adView.destroy();
 		}
 	}
-	
+
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
-		Gallery pictureList = (Gallery) findViewById(R.id.pictureList);
-		savedInstanceState.putInt("POSITION", pictureList.getSelectedItemPosition());
-		Log.d(TAG,"position "+pictureList.getSelectedItemPosition());
+		savedInstanceState.putInt("POSITION", currentPosition);
+		Log.d(TAG, "position " + currentPosition);
 		super.onSaveInstanceState(savedInstanceState);
 	}
-	
+
 }
